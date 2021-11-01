80065eec52ef705b48b25558eb51281250418da0
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-11-01 15:24:36 +0100

    better interactive interface

--- src/kamilalisp/start/Main.java
+++ src/kamilalisp/start/Main.java
@@ -1,15 +1,32 @@
 package kamilalisp.start;
 
 import kamilalisp.api.Evaluation;
+import kamilalisp.data.Atom;
 import kamilalisp.data.Environment;
 
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+    private static void banner() {
+        System.out.println("KamilaLisp v 1.0 - Copyright (C) Kamila Szewczyk, 2021.");
+        System.out.println("Interactive mode.");
+        System.out.println();
+    }
+
+    private static void prompt() {
+        System.out.print("% ");
+        System.out.flush();
+    }
+
     public static void main(String[] args) {
         Environment env = Evaluation.createDefaultEnv();
         Scanner s = new Scanner(System.in);
-        while(s.hasNextLine())
-            Evaluation.evalString(env, s.nextLine());
+        banner(); prompt();
+        while(s.hasNextLine()) {
+            List<Atom> result = Evaluation.evalString(env, s.nextLine());
+            result.forEach(System.out::println);
+            prompt();
+        }
     }
 }
