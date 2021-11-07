972b245dacdde53824c21a2daf3cb5df19471574
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-11-07 14:02:29 +0100

    solution to problem 2

---
+++ euler/p2.lisp
@@ -0,0 +1,14 @@
+
+(def fib (memo (monad (eager (
+    if [x < 2]
+        x
+        [(fib [x - 1]) + (fib [x - 2])])))))
+
+; Alternative iterative solution if consecutive fibonacci numbers
+; weren't desired:
+
+; (defun fib4 (n) (nth 0 (
+;     iterate n (lambda (x) (
+;         tie (nth 1 x) [(nth 0 x) + (nth 1 x)])) '(0 1))))
+
+(sum (filter #(& #(~ $(% _ 2)) $(> 4000000)) (map fib (iota 40))))
