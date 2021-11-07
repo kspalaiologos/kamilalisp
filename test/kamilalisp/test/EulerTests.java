bcc63a24c2f0c85b2ffb05c7e031f164699ac497
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-11-07 15:24:48 +0100

    test problem 2. java without raw string literals is incredibly ugly.

--- test/kamilalisp/test/EulerTests.java
+++ test/kamilalisp/test/EulerTests.java
@@ -18,4 +18,14 @@ public class EulerTests {
         assertEquals(Evaluation.evalString(env, "(def divides $(= 0)@%)\n" +
                 "(sum@filter (lambda (x) (| (divides x 5) (divides x 3))) (iota 1000))").get(1).getNumber().get(), new BigDecimal("233168"));
     }
+
+    @Test
+    void problem2() {
+        Environment env = Evaluation.createDefaultEnv();
+        assertEquals(Evaluation.evalString(env, "(def fib (memo (monad (seq (\n" +
+                "    if [x < 2]\n" +
+                "        x\n" +
+                "        [(fib [x - 1]) + (fib [x - 2])])))))" +
+                "(sum@filter #(& ~@$(% _ 2) $(> 4000000)) (map fib (iota 40)))").get(1).getNumber().get(), new BigDecimal("4613732"));
+    }
 }
