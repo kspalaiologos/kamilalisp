7e4e74df3d6f0bcde7fff2bfdaf2def3ea715aa7
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-11-08 17:28:17 +0100

    logarithms for complex numbers

--- src/kamilalisp/libs/math/Logarithm.java
+++ src/kamilalisp/libs/math/Logarithm.java
@@ -1,8 +1,11 @@
 package kamilalisp.libs.math;
 
+import ch.obermuhlner.math.big.BigComplex;
+import ch.obermuhlner.math.big.BigComplexMath;
 import ch.obermuhlner.math.big.BigDecimalMath;
 import kamilalisp.data.*;
 
+import java.math.BigDecimal;
 import java.math.MathContext;
 import java.util.List;
 
@@ -13,15 +16,36 @@ public class Logarithm {
             public Atom apply(Executor env, List<Atom> arguments) {
                 if(arguments.size() == 1)
                     return new Atom(new LbcSupplier<>(() -> {
-                        arguments.get(0).guardType("First argument to 'log'.", Type.NUMBER);
-                        return BigDecimalMath.log10(arguments.get(0).getNumber().get(), MathContext.DECIMAL128);
+                        arguments.get(0).guardType("First argument to 'log'.", Type.NUMBER, Type.COMPLEX);
+                        if(arguments.get(0).getType() == Type.NUMBER) {
+                            return BigDecimalMath.log10(arguments.get(0).getNumber().get(), MathContext.DECIMAL128);
+                        } else {
+                            BigComplex log10 = BigComplexMath.log(BigComplex.valueOf(new BigDecimal(10), new BigDecimal(0)), MathContext.DECIMAL128);
+                            return BigComplexMath.log(arguments.get(0).getComplex().get(), MathContext.DECIMAL128).divide(log10, MathContext.DECIMAL128);
+                        }
                     }));
                 else if(arguments.size() == 2)
                     return new Atom(new LbcSupplier<>(() -> {
-                        arguments.get(0).guardType("First argument to 'log'.", Type.NUMBER);
-                        arguments.get(1).guardType("Second argument to 'log'.", Type.NUMBER);
-                        return BigDecimalMath.log(arguments.get(1).getNumber().get(), MathContext.DECIMAL128)
-                                .divide(BigDecimalMath.log(arguments.get(0).getNumber().get(), MathContext.DECIMAL128), MathContext.DECIMAL128);
+                        arguments.get(0).guardType("First argument to 'log'.", Type.NUMBER, Type.COMPLEX);
+                        arguments.get(1).guardType("Second argument to 'log'.", Type.NUMBER, Type.COMPLEX);
+                        if(arguments.get(0).getType() == Type.NUMBER && arguments.get(1).getType() == Type.NUMBER) {
+                            return BigDecimalMath.log(arguments.get(1).getNumber().get(), MathContext.DECIMAL128)
+                                    .divide(BigDecimalMath.log(arguments.get(0).getNumber().get(), MathContext.DECIMAL128), MathContext.DECIMAL128);
+                        } else {
+                            BigComplex arg1;
+                            if(arguments.get(0).getType() == Type.NUMBER)
+                                arg1 = BigComplex.valueOf(BigDecimalMath.log(arguments.get(0).getNumber().get(), MathContext.DECIMAL128), new BigDecimal(0));
+                            else
+                                arg1 = BigComplexMath.log(arguments.get(0).getComplex().get(), MathContext.DECIMAL128);
+
+                            BigComplex arg2;
+                            if(arguments.get(1).getType() == Type.NUMBER)
+                                arg2 = BigComplex.valueOf(BigDecimalMath.log(arguments.get(1).getNumber().get(), MathContext.DECIMAL128), new BigDecimal(0));
+                            else
+                                arg2 = BigComplexMath.log(arguments.get(1).getComplex().get(), MathContext.DECIMAL128);
+
+                            return arg1.divide(arg2, MathContext.DECIMAL128);
+                        }
                     }));
                 else
                     throw new Error("Invalid invocation to 'log'.");
