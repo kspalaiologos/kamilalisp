d8cd940bbf1ae0f6eb4c0aeef6ac5c330a40f50e
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-11-09 19:19:36 +0100

    reading and evaluating atom; settle on an input model & side effects

--- src/kamilalisp/libs/IOLib.java
+++ src/kamilalisp/libs/IOLib.java
@@ -1,15 +1,16 @@
 package kamilalisp.libs;
 
+import kamilalisp.api.Evaluation;
 import kamilalisp.data.*;
 
 import java.util.List;
+import java.util.Scanner;
 
 public class IOLib {
     public static void install(Environment env) {
         env.push("println", new Atom(new Closure() {
             @Override
             public Atom apply(Executor env, List<Atom> arguments) {
-                // XXX: REMOVE AND REPLACE WITH IO MONAD
                 if(arguments.size() != 1)
                     throw new Error("'println' expects exactly one argument");
                 if(arguments.get(0).getType() == Type.STRING_CONSTANT)
@@ -17,6 +18,14 @@ public class IOLib {
                 else
                     System.out.println(arguments.get(0).toString());
                 return arguments.get(0);
+            }
+        }));
+
+        env.push("read-eval", new Atom(new Closure() {
+            @Override
+            public Atom apply(Executor env, List<Atom> arguments) {
+                Scanner s = new Scanner(System.in);
+                return Evaluation.evalAtom(env.env, s.nextLine());
             }
         }));
     }
