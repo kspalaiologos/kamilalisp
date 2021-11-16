b10cded8e399503761fe7ab9bd8f83b4adc86a2c
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-11-16 17:46:40 +0100

    updates

--- euler/p36.lisp
+++ euler/p36.lisp
@@ -1,9 +1,9 @@
 
 (def is-p? #(= #0 reverse))
 (defun cr (x) is-p?@$(encode x))
-(filter #(& (cr 10) (cr 2)) (:odd-f@range 0 500000))
+(println@filter #(& (cr 10) (cr 2)) (:odd-f@range 0 500000))
 
 
-(def c (foldl1' + (:to-string@range 1 100000)))
+(def c (foldl1' + (:(id seq@to-string)@range 1 500000)))
 (foldl1' * (index '(1000000 100000 10000 1000 100 10 1) c))
 
