418b40e66da2c66944b1dbadbe5b091ef27bfaa0
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-12-01 21:51:37 +0100

    matrix power.

--- src/kamilalisp/libs/primitives/math/Power.java
+++ src/kamilalisp/libs/primitives/math/Power.java
@@ -5,6 +5,7 @@ import ch.obermuhlner.math.big.BigComplexMath;
 import ch.obermuhlner.math.big.BigDecimalMath;
 import kamilalisp.data.*;
 import kamilalisp.libs.math.Constant;
+import kamilalisp.matrix.Matrix;
 
 import java.math.BigDecimal;
 import java.util.List;
@@ -15,13 +16,22 @@ public class Power implements Closure {
     }
 
     public static Atom power(Atom a1, Atom a2, Executor env) {
-        a1.guardType("First argument to '**'", Type.NUMBER, Type.COMPLEX);
+        a1.guardType("First argument to '**'", Type.NUMBER, Type.COMPLEX, Type.MATRIX);
         a2.guardType("Second argument to '**'", Type.NUMBER, Type.COMPLEX);
         if(a1.getType() == Type.NUMBER && a2.getType() == Type.NUMBER) {
-            if(a2.getNumber().get().stripTrailingZeros().scale() > 0)
+            if (a2.getNumber().get().stripTrailingZeros().scale() > 0)
                 return new Atom(BigDecimalMath.pow(a1.getNumber().get(), a2.getNumber().get(), Constant.getFr(env.env)));
             else
                 return new Atom(a1.getNumber().get().pow(a2.getNumber().get().intValue()));
+        } else if(a1.getType() == Type.MATRIX) {
+            int power = a2.getNumber().get().intValue();
+            Matrix mat = a1.getMatrix().get();
+            Matrix base = mat;
+            if(power == 0)
+                return new Atom(Matrix.of((x, y) -> new Atom(BigDecimal.ZERO), mat.getRows(), mat.getCols()));
+            for(int i = 1; i < power; i++)
+                mat = Product.mul2(new Atom(mat), new Atom(base)).getMatrix().get();
+            return new Atom(mat);
         } else {
             BigComplex a, b;
             if(a1.getType() == Type.NUMBER) { a = BigComplex.valueOf(a1.getNumber().get(), BigDecimal.ZERO); }
