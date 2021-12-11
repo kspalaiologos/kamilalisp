afaaccc2d4a944b2712737d3bfcb362f0f04a6c7
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-12-11 14:07:04 +0100

    day 2 problem 2

---
+++ aoc2021/2-2.lisp
@@ -0,0 +1,7 @@
+
+(def data \regex-split (ucs '(10)) \file-get "aoc2021/2-1.in")
+(def x \map parse-num@car@str-explode@reverse data)
+(def dir \index-of (str-join@:car@:str-explode data) "ufd")
+(def aim \scanl1' + \:* x \:pred dir)
+(def fwd \:$(= 1) dir)
+(println@* (sum@:* fwd x) \sum@:* fwd \:* aim x)
