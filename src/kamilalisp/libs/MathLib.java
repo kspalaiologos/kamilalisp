ac694c9ed6742c760293cc4dcc76f58bd89257f1
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-12-16 20:42:24 +0100

    clean up the code for & and |

--- src/kamilalisp/libs/MathLib.java
+++ src/kamilalisp/libs/MathLib.java
@@ -134,56 +134,7 @@ public class MathLib {
 
         env.push("lcm", DyadicMultivariateFunction.of(new LCM()));
 
-        env.push("~", DyadicMultivariateFunction.of(new Closure() {
-            public String reverseCase(String text) {
-                return text.chars()
-                        .map(c -> Character.isUpperCase(c) ?
-                                Character.toLowerCase(c) :
-                                Character.toUpperCase(c))
-                        .collect(
-                                StringBuilder::new,
-                                StringBuilder::appendCodePoint,
-                                StringBuilder::append)
-                        .toString();
-            }
-
-            public Atom neg1(Atom a) {
-                a.guardType("First argument to monadic '~'", Type.NUMBER, Type.STRING_CONSTANT);
-
-                if(a.getType() == Type.NUMBER) {
-                    return new Atom(BigDecimal.valueOf(a.getNumber().get().compareTo(BigDecimal.ZERO) == 0 ? 1 : 0));
-                } else if(a.getType() == Type.STRING_CONSTANT) {
-                    return new Atom(new StringConstant(reverseCase(a.getStringConstant().get().get())));
-                }
-
-                // Should never get here.
-                return null;
-            }
-
-            public Atom neg2(Atom a, Atom b) {
-                a.guardType("First argument to dyadic '~'", Type.LIST);
-                if(b.getType() == Type.LIST) {
-                    List<Atom> clone = new LinkedList<>(a.getList().get());
-                    clone.removeIf(x -> a.getList().get().contains(x.get().get()));
-                    return new Atom(clone);
-                } else {
-                    List<Atom> clone = new LinkedList<>(a.getList().get());
-                    clone.removeIf(x -> x.get().get().equals(b.get().get()));
-                    return new Atom(clone);
-                }
-            }
-
-            @Override
-            public Atom apply(Executor env, List<Atom> arguments) {
-                if(arguments.size() == 0)
-                    throw new Error("Invalid invocation to '~'.");
-                else if(arguments.size() == 1)
-                    return new Atom(new LbcSupplier(() -> neg1(arguments.get(0)).get().get()));
-                else if(arguments.size() == 2)
-                    return new Atom(new LbcSupplier(() -> neg2(arguments.get(0), arguments.get(1)).get().get()));
-                throw new Error("Invalid invocation to '~'.");
-            }
-        }));
+        env.push("~", DyadicMultivariateFunction.of(new Tilde()));
 
         env.push("<", new Atom(new Closure() {
             @Override
@@ -311,35 +262,7 @@ public class MathLib {
             }
         }));
 
-        env.push("&", new Atom(new Closure() {
-            @Override
-            public Atom apply(Executor env, List<Atom> arguments) {
-                if(arguments.size() != 2)
-                    throw new Error("Invalid invocation to '&'.");
-                return new Atom(new LbcSupplier<>(() -> {
-                    if(arguments.get(0).getType() == Type.NUMBER && arguments.get(1).getType() == Type.NUMBER) {
-                        return new BigDecimal(
-                                arguments
-                                        .get(0)
-                                        .getNumber()
-                                        .get()
-                                        .toBigInteger()
-                                        .and(arguments.get(1).getNumber().get().toBigInteger()));
-                    } else if(arguments.get(0).getType() == Type.STRING_CONSTANT && arguments.get(1).getType() == Type.STRING_CONSTANT) {
-                        Set<Integer> result = new LinkedHashSet<>();
-                        result.addAll(arguments.get(0).getStringConstant().get().get().codePoints().boxed().collect(Collectors.toList()));
-                        result.retainAll(arguments.get(1).getStringConstant().get().get().codePoints().boxed().collect(Collectors.toList()));
-                        return new StringConstant(result.stream().collect(StringBuilder::new, StringBuilder::appendCodePoint, StringBuilder::append).toString());
-                    } else if(arguments.get(0).getType() == Type.LIST && arguments.get(1).getType() == Type.LIST) {
-                        Set<Atom> result = new LinkedHashSet<>();
-                        result.addAll(arguments.get(0).getList().get());
-                        result.retainAll(arguments.get(1).getList().get());
-                        return result.stream().collect(Collectors.toList());
-                    } else
-                        throw new Error("Invalid invocation to '&'. Expected two numbers, two strings or two lists.");
-                }));
-            }
-        }));
+        env.push("&", new Atom(new And()));
 
         env.push("xor", new Atom(new Closure() {
             @Override
