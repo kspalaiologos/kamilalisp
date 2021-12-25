0ef98ca5a0f95a96fce9ad68bc56aa7a43d8ab16
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-12-25 14:45:13 +0100

    error.hpp: add a rationale for not using std::source_location

--- error.hpp
+++ error.hpp
@@ -3,11 +3,17 @@
 #define __ERROR_HPP
 
 #include <exception>
+#include <string>
+#include <stdexcept>
+
+// Not using std::source_location because it's not available on all
+// reasonably modern compilers.
 
 #define __stringify(msg) #msg
 #define __stringify_cpp_sucks(msg) __stringify(msg)
 #define location \
     std::string(__FILE__ ":" __stringify_cpp_sucks(__LINE__) " - ")
+
 #define kl_error(msg) \
     throw std::runtime_error(location + (msg))
 #define kl_recoverable_error(msg) \
