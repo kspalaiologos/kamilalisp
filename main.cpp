f8af978c4bde38a32cd0de981166848f65e0bb5e
Kamila Szewczyk <kspalaiologos@gmail.com>
2021-12-25 14:06:02 +0100

    syntax highlighting & replxx bug workaround

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "executor.hpp"
 #include "reader/parser.hpp"
 #include "replxx/replxx.hxx"
+#include "replxx/replxx_impl.hxx"
 #include "error.hpp"
 
 #include <sstream>
@@ -11,6 +12,84 @@
 #include <fstream>
 #include <locale>
 #include <codecvt>
+
+void highlighter(std::string const & input, replxx::Replxx::colors_t & c) {
+    static std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> cvt;
+    static std::shared_ptr<environment> env = environment::create_default_env();
+    std::wstring data = cvt.from_bytes(input.c_str());
+    std::vector<token> lexemes = lex_greedy(data);
+    try {
+        for (auto & lexeme : lexemes) {
+            switch (lexeme.type) {
+                case token_type::TOKEN_ID: {
+                    std::wstring s = std::get<std::wstring>(*lexeme.content);
+                    if(env->has(s)) {
+                        for(std::size_t i = 0; i < s.size(); i++)
+                            c.at(i + lexeme.loc) = replxx::Replxx::Color::MAGENTA;
+                    }
+                    break;
+                }
+                case token_type::TOKEN_HEX:
+                case token_type::TOKEN_FPU:
+                case token_type::TOKEN_COMPLEX:
+                case token_type::TOKEN_INT:
+                case token_type::TOKEN_BIN: {
+                    std::string s = std::get<std::string>(*lexeme.content);
+                    for(std::size_t i = 0; i < s.size(); i++)
+                        c.at(i + lexeme.loc) = replxx::Replxx::Color::YELLOW;
+                    break;
+                }
+                case token_type::TOKEN_STR: {
+                    std::wstring s = std::get<std::wstring>(*lexeme.content);
+                    for(std::size_t i = 0; i < s.size(); i++)
+                        c.at(i + lexeme.loc) = replxx::Replxx::Color::GREEN;
+                    break;
+                }
+                case token_type::TOKEN_NIL: {
+                    for(std::size_t i = 0; i < 3; i++)
+                        c.at(lexeme.loc + i) = replxx::Replxx::Color::RED;
+                    break;
+                }
+                case token_type::TOKEN_TACK: {
+                    std::string s = std::get<std::string>(*lexeme.content);
+                    for(std::size_t i = 0; i < s.size() + 1; i++)
+                        c.at(i + lexeme.loc) = replxx::Replxx::Color::BROWN;
+                    break;
+                }
+                case token_type::TOKEN_ATOP:
+                case token_type::TOKEN_BIND:
+                case token_type::TOKEN_FORK:
+                case token_type::TOKEN_LBRA:
+                case token_type::TOKEN_RBRA:
+                case token_type::TOKEN_QUOT:
+                case token_type::TOKEN_LPAR:
+                case token_type::TOKEN_RPAR:
+                case token_type::TOKEN_SLASH:
+                case token_type::TOKEN_OVER:
+                case token_type::TOKEN_MAP: {
+                    c.at(lexeme.loc) = replxx::Replxx::Color::BLUE;
+                    break;
+                }
+            }
+        }
+    } catch(std::exception const& e) {
+        std::wcerr << "Exception thrown." << e.what() << std::endl;
+    }
+}
+
+auto res(replxx::Replxx & instance) {
+    return [&instance](char32_t c) -> replxx::Replxx::ACTION_RESULT {
+        (void) c;
+        static std::wstring_convert<std::codecvt_utf8<wchar_t>, wchar_t> cvt;
+        const char * text = instance.get_state().text();
+        std::wstring data = cvt.from_bytes(text);
+        try { parse(data); } catch(std::domain_error const&) {
+            instance.invoke(replxx::Replxx::ACTION::NEW_LINE, '\n');
+            return replxx::Replxx::ACTION_RESULT::CONTINUE;
+        } catch(...) { }
+        return replxx::Replxx::ACTION_RESULT::RETURN;
+    };
+}
 
 int main(int argc, char * argv[]) {
     std::ios_base::sync_with_stdio(false);
@@ -31,6 +110,15 @@ int main(int argc, char * argv[]) {
         std::wcout
             << "KamilaLisp v0.2 - Interactive Mode." << std::endl
             << "Copyright (C) 2021-2022 Kamila Szewczyk" << std::endl;
+        
+        // Bug workaround.
+        // https://github.com/AmokHuginnsson/replxx/issues/137
+        repl.history_add("");
+        repl.install_window_change_handler();
+        repl.set_highlighter_callback(highlighter);
+        repl.set_indent_multiline(true);
+
+        repl.bind_key(replxx::Replxx::KEY::ENTER + 0, res(repl));
 
         while(true) {
             const char * in = repl.input("--> ");
