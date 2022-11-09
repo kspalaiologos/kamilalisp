c2c7a678d771d96b8d7ef08b15b181ade128a0af
Kamila Szewczyk <kspalaiologos@gmail.com>
2022-11-09 15:31:22 +0100

    reverse domain

--- src/main/java/palaiologos/kamilalisp/runtime/array/Reverse.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/Reverse.java
@@ -15,16 +15,24 @@ public class Reverse extends PrimitiveFunction implements Lambda {
         }
 
         if(args.size() == 1) {
-            args.get(0).assertTypes(Type.LIST);
-            if(args.get(0).getList().size() <= 1)
+            args.get(0).assertTypes(Type.LIST, Type.STRING);
+            if((args.get(0).getType() == Type.LIST && args.get(0).getList().size() <= 1)
+            || (args.get(0).getType() == Type.STRING && args.get(0).getString().length() <= 1))
                 return args.get(0);
-            return new Atom(Lists.reverse(args.get(0).getList()));
+            if(args.get(0).getType() == Type.LIST)
+                return new Atom(Lists.reverse(args.get(0).getList()));
+            else
+                return new Atom(new StringBuilder(args.get(0).getString()).reverse().toString());
         } else {
-            args.forEach(a -> a.assertTypes(Type.LIST));
+            args.forEach(a -> a.assertTypes(Type.LIST, Type.STRING));
             return new Atom(args.stream().map(l -> {
-                if(l.getList().size() <= 1)
+                if((l.getType() == Type.LIST && l.getList().size() <= 1)
+                || (l.getType() == Type.STRING && l.getString().length() <= 1))
                     return l;
-                return new Atom(Lists.reverse(l.getList()));
+                if(l.getType() == Type.LIST)
+                    return new Atom(Lists.reverse(l.getList()));
+                else
+                    return new Atom(new StringBuilder(l.getString()).reverse().toString());
             }).collect(Collectors.toList()));
         }
     }
