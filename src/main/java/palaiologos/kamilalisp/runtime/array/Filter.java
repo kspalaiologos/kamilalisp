f1456d80eec40cd38db48ef11a9f5ea91be99d2d
Kamila Szewczyk <kspalaiologos@gmail.com>
2022-11-11 10:24:32 +0100

    filter on `strings`.

--- src/main/java/palaiologos/kamilalisp/runtime/array/Filter.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/Filter.java
@@ -1,6 +1,7 @@
 package palaiologos.kamilalisp.runtime.array;
 
 import palaiologos.kamilalisp.atom.*;
+import palaiologos.kamilalisp.error.TypeError;
 
 import java.util.List;
 
@@ -9,8 +10,17 @@ public class Filter extends PrimitiveFunction implements Lambda {
     public Atom apply(Environment env, List<Atom> args) {
         assertArity(args, 2);
         Callable reductor = args.get(0).getCallable();
-        List<Atom> list = args.get(1).getList();
-        return new Atom(list.stream().filter(x -> Evaluation.evaluate(env, reductor, List.of(x)).coerceBool()).toList());
+        if(args.get(1).getType() == Type.LIST) {
+            List<Atom> list = args.get(1).getList();
+            return new Atom(list.stream().filter(x -> Evaluation.evaluate(env, reductor, List.of(x)).coerceBool()).toList());
+        } else if(args.get(1).getType() == Type.STRING) {
+            String string = args.get(1).getString();
+            return new Atom(string.chars().filter(
+                    x -> Evaluation.evaluate(env, reductor, List.of(new Atom(String.valueOf((char) x)))).coerceBool()
+            ).mapToObj(x -> (char) x).collect(StringBuilder::new, StringBuilder::append, StringBuilder::append).toString());
+        } else {
+            throw new TypeError("Expected a list or a string as the second argument to `filter'.");
+        }
     }
 
     @Override
