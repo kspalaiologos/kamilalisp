1e137b684f305cbf9efcf44dca98db51328ce362
Kamila Szewczyk <kspalaiologos@gmail.com>
2022-11-11 18:41:11 +0100

    take: implement methods

--- src/main/java/palaiologos/kamilalisp/runtime/array/Take.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/Take.java
@@ -1,8 +1,20 @@
 package palaiologos.kamilalisp.runtime.array;
 
+import palaiologos.kamilalisp.atom.Atom;
+import palaiologos.kamilalisp.atom.Environment;
 import palaiologos.kamilalisp.atom.Lambda;
 import palaiologos.kamilalisp.atom.PrimitiveFunction;
 
+import java.util.List;
+
 public class Take extends PrimitiveFunction implements Lambda {
-    
+    @Override
+    public Atom apply(Environment env, List<Atom> args) {
+        return null;
+    }
+
+    @Override
+    protected String name() {
+        return "take";
+    }
 }
