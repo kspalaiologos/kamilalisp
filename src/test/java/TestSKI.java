687a197a2e8b10c970f50cc2c10e94a67b50d2de
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-01-27 11:12:54 +0100

    SKI calculus test

---
+++ src/test/java/TestSKI.java
@@ -0,0 +1,22 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.parallel.Execution;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
+
+public class TestSKI {
+    @Test
+    void testSki() {
+        String code = """
+                (defun SKI x
+                   (match x
+                      ((((S 'x) 'y) 'z) (tie (tie x z) (tie y z)))
+                      (((K 'x) 'y) x)
+                      ((I 'x) x)
+                      (('x 'y) (tie (SKI x) (SKI y)))
+                      ('x x)))
+
+                (converge SKI '(((S I) I) K))""";
+        assertEquals(Common.runCode(code), Common.runCode("#0 '(K K)"));
+    }
+}
