0abc61d70aacb194995f73ce14457d12afc43409
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-01-30 18:37:52 +0100

    Sieve of Eratosthenes

--- src/main/java/palaiologos/kamilalisp/runtime/meta/LetSeq.java
+++ src/main/java/palaiologos/kamilalisp/runtime/meta/LetSeq.java
@@ -15,10 +15,11 @@ public class LetSeq extends PrimitiveFunction implements SpecialForm {
             if (arg.getType() == Type.LIST && !arg.getList().isEmpty()) {
                 if (arg.getList().get(0).getType() == Type.IDENTIFIER) {
                     switch (Identifier.of(arg.getList().get(0).getIdentifier())) {
+                        case "⍥←":
                         case "defun": {
                             List<Atom> declaration = arg.getList();
                             if (declaration.get(1).getType() != Type.IDENTIFIER) {
-                                throw new RuntimeException("Expected identifier in `def' declaration in `let-seq'.");
+                                throw new RuntimeException("Expected identifier in `defun' declaration in `let-seq'.");
                             }
                             String name = Identifier.of(declaration.get(1).getIdentifier());
                             Atom lambdaArgs = declaration.get(2);
@@ -38,10 +39,17 @@ public class LetSeq extends PrimitiveFunction implements SpecialForm {
                             env.set(name, value);
                             continue;
                         }
-                        case "discard": {
-                            // Discard and ignore.
-                            Evaluation.evaluate(env, arg);
-                            continue;
+                        case "↩":
+                        case "case": {
+                            // Yield if true.
+                            List<Atom> declaration = arg.getList();
+                            Atom condition = declaration.get(1);
+                            Atom value = declaration.get(2);
+                            if (Evaluation.evaluate(env, condition).coerceBool()) {
+                                return Evaluation.evaluate(env, value);
+                            } else {
+                                continue;
+                            }
                         }
                     }
                 }
