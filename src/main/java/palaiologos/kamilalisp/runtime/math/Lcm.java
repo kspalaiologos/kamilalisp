8d324254c3d040e15a7e0744bb386b5a7991e93a
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-01-31 14:50:26 +0100

    gamma, lcm, gcd unit test

--- src/main/java/palaiologos/kamilalisp/runtime/math/Lcm.java
+++ src/main/java/palaiologos/kamilalisp/runtime/math/Lcm.java
@@ -5,6 +5,8 @@ import com.google.common.collect.Streams;
 import palaiologos.kamilalisp.atom.*;
 import palaiologos.kamilalisp.error.ArrayError;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
 import java.util.List;
 
 public class Lcm extends PrimitiveFunction implements Lambda {
@@ -15,9 +17,13 @@ public class Lcm extends PrimitiveFunction implements Lambda {
             return new Atom(Streams.zip(a.getList().stream(), b.getList().stream(), (x, y) -> min2(e, x, y)).toList());
         } else if (a.getType() == Type.INTEGER && b.getType() == Type.INTEGER) {
             Atom gcd = Gcd.gcd2(e, a, b);
+            if(b.getInteger().compareTo(BigInteger.ZERO) == 0)
+                return new Atom(BigInteger.ZERO);
             return new Atom(a.getInteger().multiply(b.getInteger().divide(gcd.getInteger())));
         } else if ((a.getType() == Type.REAL || a.getType() == Type.INTEGER) && (b.getType() == Type.REAL || b.getType() == Type.INTEGER)) {
             Atom gcd = Gcd.gcd2(e, a, b);
+            if(gcd.getReal().compareTo(BigDecimal.ZERO) == 0)
+                return new Atom(BigDecimal.ZERO);
             return new Atom(a.getReal().multiply(b.getReal().divide(gcd.getReal(), e.getMathContext())));
         } else if (a.getType() == Type.LIST && b.isNumeric()) {
             return new Atom(a.getList().stream().map(x -> min2(e, x, b)).toList());
@@ -26,6 +32,10 @@ public class Lcm extends PrimitiveFunction implements Lambda {
         } else {
             Atom gcd = Gcd.gcd2(e, a, b);
             BigComplex result;
+            if (gcd.getType() == Type.COMPLEX && gcd.getReal().compareTo(BigDecimal.ZERO) == 0)
+                return new Atom(BigComplex.valueOf(BigDecimal.ZERO, BigDecimal.ZERO));
+            else if(gcd.getComplex().equals(BigComplex.valueOf(BigDecimal.ZERO, BigDecimal.ZERO)))
+                return new Atom(BigComplex.valueOf(BigDecimal.ZERO, BigDecimal.ZERO));
             if (gcd.getType() == Type.COMPLEX)
                 result = a.getComplex().multiply(b.getComplex().divide(gcd.getComplex(), e.getMathContext()));
             else
