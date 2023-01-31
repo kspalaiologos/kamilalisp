ec15df768b30e7addf0863ba5a5951376a19db68
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-01-31 18:55:29 +0100

    expand upon the C comment section

--- src/main/java/palaiologos/kamilalisp/runtime/Index.java
+++ src/main/java/palaiologos/kamilalisp/runtime/Index.java
@@ -37,22 +37,47 @@ public class Index implements SpecialForm {
         } else {
             ix = Evaluation.evaluate(env, new Atom(args));
         }
-        if (ix.getType() == Type.INTEGER) {
-            return indexedAtom.getList().get(ix.getInteger().intValueExact());
-        } else if (ix.getType() == Type.REAL) {
-            return indexedAtom.getList().get(ix.getReal().intValue());
-        } else if (ix.getType() == Type.LIST) {
-            return new Atom(ix.getList().stream().map(x -> {
-                if (x.getType() == Type.INTEGER) {
-                    return indexedAtom.getList().get(x.getInteger().intValueExact());
-                } else if (x.getType() == Type.REAL) {
-                    return indexedAtom.getList().get(x.getReal().intValue());
-                } else {
-                    throw new RuntimeException("Can't index with type " + x.getType());
+
+        if(indexedAtom.getType() == Type.LIST) {
+            if (ix.getType() == Type.INTEGER) {
+                return indexedAtom.getList().get(ix.getInteger().intValueExact());
+            } else if (ix.getType() == Type.REAL) {
+                return indexedAtom.getList().get(ix.getReal().intValue());
+            } else if (ix.getType() == Type.LIST) {
+                return new Atom(ix.getList().stream().map(x -> {
+                    if (x.getType() == Type.INTEGER) {
+                        return indexedAtom.getList().get(x.getInteger().intValueExact());
+                    } else if (x.getType() == Type.REAL) {
+                        return indexedAtom.getList().get(x.getReal().intValue());
+                    } else {
+                        throw new RuntimeException("Can't index with type " + x.getType());
+                    }
+                }).toList());
+            } else {
+                throw new RuntimeException("Can't index with type " + ix.getType());
+            }
+        } else if(indexedAtom.getType() == Type.STRING) {
+            if (ix.getType() == Type.INTEGER) {
+                return new Atom(String.valueOf(indexedAtom.getString().charAt(ix.getInteger().intValueExact())));
+            } else if (ix.getType() == Type.REAL) {
+                return new Atom(String.valueOf(indexedAtom.getString().charAt(ix.getReal().intValue())));
+            } else if (ix.getType() == Type.LIST) {
+                StringBuilder sb = new StringBuilder();
+                for(Atom a : ix.getList()) {
+                    if (a.getType() == Type.INTEGER) {
+                        sb.append(indexedAtom.getString().charAt(a.getInteger().intValueExact()));
+                    } else if (a.getType() == Type.REAL) {
+                        sb.append(indexedAtom.getString().charAt(a.getReal().intValue()));
+                    } else {
+                        throw new RuntimeException("Can't index with type " + a.getType());
+                    }
                 }
-            }).toList());
+                return new Atom(sb.toString());
+            } else {
+                throw new RuntimeException("Can't index with type " + ix.getType());
+            }
         } else {
-            throw new RuntimeException("Can't index with type " + ix.getType());
+            throw new RuntimeException("Can't index into type " + indexedAtom.getType());
         }
     }
 
