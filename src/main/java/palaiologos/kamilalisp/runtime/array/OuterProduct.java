bbd2d93ed9e77593117fd83b3884a2e91e99e252
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-02 23:17:19 +0100

    reimplement outer-product and rip out the section about inner product implementation from the book - outdated.

--- src/main/java/palaiologos/kamilalisp/runtime/array/OuterProduct.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/OuterProduct.java
@@ -4,22 +4,39 @@ import com.google.common.collect.Lists;
 import palaiologos.kamilalisp.atom.*;
 import palaiologos.kamilalisp.error.TypeError;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.function.BiFunction;
 import java.util.stream.Collectors;
 
 public class OuterProduct extends PrimitiveFunction implements Lambda {
-    public Atom op2(Atom a, Atom b) {
+    private static Tie tie = new Tie();
+
+    static <T, R> List<List<R>> op(BiFunction<T, T, R> f, List<T> a, List<T> b) {
+        List<List<R>> results = new ArrayList<>();
+        for (T x : a) {
+            List<R> result = new ArrayList<>();
+            for (T y : b) {
+                result.add(f.apply(x, y));
+            }
+            results.add(result);
+        }
+        return results;
+    }
+
+    public static Atom op2(Callable fn, Environment env, Atom a, Atom b) {
         if (a.getType() == Type.LIST && b.getType() == Type.LIST) {
-            return new Atom(Lists.cartesianProduct(a.getList(), b.getList()).stream().map(Atom::new).toList());
+            return new Atom(op((x, y) -> fn.apply(env, List.of(x, y)), a.getList(), b.getList()).stream().map(Atom::new).toList());
         } else if (a.getType() == Type.STRING && b.getType() == Type.STRING) {
-            return new Atom(Lists.cartesianProduct(
+            return new Atom(op(
+                    (x, y) -> fn.apply(env, List.of(new Atom(String.valueOf(x)), new Atom(String.valueOf(y)))),
                     a.getString().chars().mapToObj(x -> (char) x).toList(),
                     b.getString().chars().mapToObj(x -> (char) x).toList()
             ).stream().map(x -> new Atom(x.stream().map(Object::toString).collect(Collectors.joining()))).toList());
         } else if (a.getType() == Type.LIST && b.getType() != Type.LIST) {
-            return new Atom(Lists.cartesianProduct(a.getList(), List.of(b)).stream().map(Atom::new).toList());
+            return new Atom(op((x, y) -> fn.apply(env, List.of(x, y)), a.getList(), List.of(b)).stream().map(Atom::new).toList());
         } else if (a.getType() != Type.LIST && b.getType() == Type.LIST) {
-            return new Atom(Lists.cartesianProduct(List.of(a), b.getList()).stream().map(Atom::new).toList());
+            return new Atom(op((x, y) -> fn.apply(env, List.of(x, y)), List.of(a), b.getList()).stream().map(Atom::new).toList());
         } else {
             return new Atom(List.of(a, b));
         }
@@ -31,7 +48,15 @@ public class OuterProduct extends PrimitiveFunction implements Lambda {
             throw new TypeError("outer-product called with too few arguments.");
         }
 
-        return args.stream().reduce(this::op2).get();
+        if(args.get(0).getType() == Type.CALLABLE) {
+            if(args.size() <= 2) {
+                throw new TypeError("outer-product called with too few arguments.");
+            }
+
+            return args.stream().skip(1).reduce((x, y) -> op2(args.get(0).getCallable(), env, x, y)).get();
+        } else {
+            return args.stream().reduce((x, y) -> op2(tie, env, x, y)).get();
+        }
     }
 
     @Override
