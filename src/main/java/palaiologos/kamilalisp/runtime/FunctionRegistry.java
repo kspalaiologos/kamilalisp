959aca0f1c435f80969865a95a127bbf10f4ec89
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-03 18:57:35 +0100

    phase out QR decomposition.

--- src/main/java/palaiologos/kamilalisp/runtime/FunctionRegistry.java
+++ src/main/java/palaiologos/kamilalisp/runtime/FunctionRegistry.java
@@ -21,7 +21,6 @@ import palaiologos.kamilalisp.runtime.math.prime.PrimeFactors;
 import palaiologos.kamilalisp.runtime.math.prime.PrimeNo;
 import palaiologos.kamilalisp.runtime.math.trig.*;
 import palaiologos.kamilalisp.runtime.matrix.LUDecomposition;
-import palaiologos.kamilalisp.runtime.matrix.QRDecomposition;
 import palaiologos.kamilalisp.runtime.matrix.Transpose;
 import palaiologos.kamilalisp.runtime.meta.*;
 import palaiologos.kamilalisp.runtime.regex.RegexMatches;
@@ -228,8 +227,6 @@ public class FunctionRegistry {
         env.setp("⎕⍉", new Atom(new Transpose()));
         env.setp("matrix:LU", new Atom(new LUDecomposition()));
         env.setp("⎕↙↗", new Atom(new LUDecomposition()));
-        env.setp("matrix:QR", new Atom(new QRDecomposition()));
-        env.setp("⎕∥↘", new Atom(new QRDecomposition()));
 
         env.setp("abs", new Atom(new Abs()));
         env.setp("bernoulli", new Atom(new Bernoulli()));
