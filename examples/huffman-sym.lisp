eba873bd9c591d10a4d63dabf6815a00fdda9d8c
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-04 22:28:03 +0100

    more concise version of the code

--- examples/huffman-sym.lisp
+++ examples/huffman-sym.lisp
@@ -1,29 +1,21 @@
 
 (defun public:huffman-encode (b) (○⊢¨
-    (def ft \:[,⍧ ⍴∘⍎⍕ ⍎] \⌸ b)
-    (defun ipq (pq el)
-        \⍠⊣ pq el \⍴? (λ x \< (⍎ x) (⍎ el)) pq)
-    (defun hstep pq
-        \↕ (= 1 \⍴ pq) pq \ipq (⍕⍕ pq)
-            \,⍧ (+ (⍎⍎ pq) (⍎∘⍎⍕ pq)) 
-                (,⍧ (⍎⍕∘⍎ pq) (⍎⍕∘⍎⍕ pq)))
-    (def htree \⍎∘⍕⍎ \→≡ hstep ft$[⍋ \⍎%[1] ft])
-    (defun tag (t c) (↕ (= 0 \⍴ t) (,⍧ t (⌽ c))
-        (⍠ (tag (⍎ t) (⍟ 0 c))
-           (tag (⍎⍕ t) (⍟ 1 c)))))
-    (def htab \⍡¨ ⍴⍴ \tag htree '⍬)
-    (def eid \∊ (⍎ htab)$[∊?%[0 1] b (⍕ htab)])
-    (def p \↑ (⌶∧ (+ (⍴ eid) 7) -8) eid)
+    (○← ft \:[,⍧ ⍴∘⍎⍕ ⍎] \⌸ b)
+    (⍥← ipq (pq el) \⍠⊣ pq el \⍴? (λ x \< (⍎ x) (⍎ el)) pq)
+    (⍥← hstep pq \↕ (= 1 \⍴ pq) pq \ipq (⍕⍕ pq)
+        \,⍧ (+ (⍎⍎ pq) (⍎∘⍎⍕ pq)) (,⍧ (⍎⍕∘⍎ pq) (⍎⍕∘⍎⍕ pq)))
+    (○← htree \⍎∘⍕⍎ \→≡ hstep ft$[⍋ \⍎%[1] ft])
+    (⍥← tag (t c) (↕ (= 0 \⍴ t) (,⍧ t (⌽ c))
+        (⍠ (tag (⍎ t) (⍟ 0 c)) (tag (⍎⍕ t) (⍟ 1 c)))))
+    (○← htab \⍡¨ ⍴⍴ \tag htree '⍬)
+    (○← eid \∊ (⍎ htab)$[∊?%[0 1] b (⍕ htab)])
+    (○← p \↑ (⌶∧ (+ (⍴ eid) 7) -8) eid)
     (,⍧ (⍴ eid) htab \:$(- _ 128)∘:$(⊥⍟ 2) \⍡ (⍉↩ (⍴ p) (↑ 8 '(1))) p)
 ))
 
 (defun public:huffman-decode (b) (○⊢¨
-    (def htab (⍎⍕ b))
-    (defun unhuf (s d) (○⊢¨
-        (def mi \⍎∘⍸∘:⍠⊂← (,⍧ s) (⍎ htab))
-        (def nd (⍠ d \,⍧ (⍎⍕ htab)$[#0 mi]))
-        (def ns (↓ (⍴ (⍎ htab)$[#0 mi]) s))
-        (↕ (= 0 \⍴ ns) nd \&0 ns nd)
-    ))
+    (○← htab (⍎⍕ b)) (⍥← unhuf (s d) (○⊢¨
+        (○← mi \⍎∘⍸∘:⍠⊂← (,⍧ s) (⍎ htab)) (○← nd (⍠ d \,⍧ (⍎⍕ htab)$[#0 mi]))
+        (○← ns (↓ (⍴ (⍎ htab)$[#0 mi]) s)) (↕ (= 0 \⍴ ns) nd \&0 ns nd)))
     (unhuf (↑ (⍎ b) \∊∘:(λ x \⌽∘↑ 8 \⌽∘⊤⍟ 2 (+ 128 x)) \⍎∘⍕⍕ b) '⍬)
 ))
