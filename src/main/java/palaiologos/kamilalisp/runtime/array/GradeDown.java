acc022b795a3b2622d340a02b84c4ff2156a1f86
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-07 23:28:28 +0100

    grade up/down optimisation

--- src/main/java/palaiologos/kamilalisp/runtime/array/GradeDown.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/GradeDown.java
@@ -11,23 +11,13 @@ public class GradeDown extends PrimitiveFunction implements Lambda {
     @Override
     public Atom apply(Environment env, List<Atom> args) {
         if (args.size() == 1) {
-            return new Atom(IntStream.range(0, args.get(0).getList().size()).boxed().sorted(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return -args.get(0).getList().get(o1).compareTo(args.get(0).getList().get(o2));
-                }
-            }).map(x -> new Atom(BigInteger.valueOf(x))).toList());
+            return new Atom(IntStream.range(0, args.get(0).getList().size()).boxed().sorted((o1, o2) -> -args.get(0).getList().get(o1).compareTo(args.get(0).getList().get(o2))).map(x -> new Atom(BigInteger.valueOf(x))).toList());
         } else if (args.size() == 2) {
             Callable reductor = args.get(1).getCallable();
-            return new Atom(IntStream.range(0, args.get(0).getList().size()).boxed().sorted(new Comparator<Integer>() {
-                @Override
-                public int compare(Integer o1, Integer o2) {
-                    return -Evaluation.evaluate(env, reductor,
-                                    List.of(args.get(0).getList().get(o1),
-                                            args.get(0).getList().get(o2)))
-                            .getInteger().intValueExact();
-                }
-            }).map(x -> new Atom(BigInteger.valueOf(x))).toList());
+            return new Atom(IntStream.range(0, args.get(0).getList().size()).boxed().sorted((o1, o2) -> -Evaluation.evaluate(env, reductor,
+                            List.of(args.get(0).getList().get(o1),
+                                    args.get(0).getList().get(o2)))
+                    .getInteger().intValueExact()).map(x -> new Atom(BigInteger.valueOf(x))).toList());
         } else {
             throw new RuntimeException("Wrong number of arguments to grade-down: expected 1 or 2, got " + args.size());
         }
