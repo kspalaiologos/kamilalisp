7a42b26434d5f9245a817fced77a57c92956b4d1
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-08 13:08:57 +0100

    test lambda multiple stmts

--- src/test/java/TestLambda.java
+++ src/test/java/TestLambda.java
@@ -1,6 +1,10 @@
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.parallel.Execution;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 import java.math.BigDecimal;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
@@ -8,6 +12,14 @@ import static org.junit.jupiter.api.parallel.ExecutionMode.CONCURRENT;
 
 @Execution(CONCURRENT)
 class TestLambda {
+    private final PrintStream standardOut = System.out;
+    private final ByteArrayOutputStream outputStreamCaptor = new ByteArrayOutputStream();
+
+    @BeforeEach
+    public void setUp() {
+        System.setOut(new PrintStream(outputStreamCaptor));
+    }
+
     @Test
     void testBasic() {
         assertEquals(Common.runCode(
@@ -60,5 +72,16 @@ class TestLambda {
                 "(def sum-all (lambda (x ...xs) (foldl + x ...xs)))"
                         + "(sum-all 1)"
         ).getReal(), new BigDecimal("1"));
+    }
+
+    @Test
+    void testMultipleStmts() {
+        Common.runCode("(defun thing (x y) (io:write x) (io:write y)) (thing 2 3)");
+        assertEquals("23", outputStreamCaptor.toString().trim());
+    }
+
+    @AfterEach
+    public void tearDown() {
+        System.setOut(standardOut);
     }
 }
