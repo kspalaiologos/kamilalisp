38f518bbe214e8f0643f197a920ed1558157093d
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-14 10:28:13 +0100

    fix parens in CAS unit tests

--- src/test/java/TestCAS.java
+++ src/test/java/TestCAS.java
@@ -11,40 +11,40 @@ class TestCAS {
     @Test
     void testBasicIntegral() {
         assertEquals(Common.runCode("""
-            cas:integral x dx
+            (cas:integral x dx)
         """).toString(), "(* (* (/ 1 2) x) x)");
 
         assertEquals(Common.runCode("""
-            cas:integral (+ x (** x 2)) dx
+            (cas:integral (+ x (** x 2)) dx)
         """).toString(), "(+ (* (/ 1 3) (** x 3)) (* (* (/ 1 2) x) x))");
     }
 
     @Test
     void testBasicIntegralTrig() {
         assertEquals(Common.runCode("""
-            cas:integral (sin x) dx
+            (cas:integral (sin x) dx)
         """).toString(), "(- (cos x))");
 
         assertEquals(Common.runCode("""
-            cas:integral (cot x) dx
+            (cas:integral (cot x) dx)
         """).toString(), "(/ (log (/ (- (+ (cos (* 2 x)) 1)) 2)) 2)");
     }
 
     @Test
     void testIntegralTrig() {
         assertEquals(Common.runCode("""
-            cas:integral (/ (+ (sin x) (cos x))) dx
+            (cas:integral (/ (+ (sin x) (cos x))) dx)
         """).toString(), "(/ (* (sqrt 2) (log (/ (- (* (- (+ (* 2 (cos x)) (* 2 (sqrt 2)))) (sin x)) (+ (* (* 2 (sqrt 2)) (cos x)) 3)) (+ (* (* 2 (cos x)) (sin x)) 1)))) 4)");
 
         assertEquals(Common.runCode("""
-            cas:integral (* (sin x) (cos x) (tan x)) dx
+            (cas:integral (* (sin x) (cos x) (tan x)) dx)
         """).toString(), "(/ (- (+ (* (cos x) (sin x)) x)) 2)");
     }
 
     @Test
     void testIntegralExpLog() {
         assertEquals(Common.runCode("""
-            cas:integral (exp (+ 1 (ln x))) dx
+            (cas:integral (exp (+ 1 (ln x))) dx)
         """).toString(), "(/ (* (* x x) e) 2)");
     }
 }
