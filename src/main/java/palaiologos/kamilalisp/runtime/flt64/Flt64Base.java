eb306c1015659e775d8b05ed65a410292eca20ee
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-17 22:04:35 +0100

    remove bessel Y from the todolist

--- src/main/java/palaiologos/kamilalisp/runtime/flt64/Flt64Base.java
+++ src/main/java/palaiologos/kamilalisp/runtime/flt64/Flt64Base.java
@@ -1612,7 +1612,7 @@ public class Flt64Base {
     public void registerFlt64(Environment env) {
         // TODO:
         // dawson-f, log-integral, fresnel-s, fresnel-c, fresnel-f, fresnel-g,
-        // Si, Co, Shi, Chi, bessel-y, bessel-i, bessel-k, hankel-h1, hankel-h2,
+        // Si, Co, Shi, Chi, bessel-i, bessel-k, hankel-h1, hankel-h2,
         // airy-bi, hypergeom-2f1, hypergeom-pfq, meijer-g, fox-h, hypergeom-1f1,
         // whittaker-m, whittaker-w, elliptic-k, elliptic-f, elliptic-e, elliptic-pi,
         // lerch-phi, dirichlet-beta, dirichlet-eta, dirichlet-lambda, barnesg, logbarnesg,
