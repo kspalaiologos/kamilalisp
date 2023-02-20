3f9ef56c91c3bcb0357d07192b4297920c518f9a
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-20 15:34:38 +0100

    IDE: clear relative frame position

--- src/main/java/palaiologos/kamilalisp/runtime/ide/IDE.java
+++ src/main/java/palaiologos/kamilalisp/runtime/ide/IDE.java
@@ -25,6 +25,7 @@ public class IDE {
         frame = new JFrame("KamilaLisp");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setSize(800, 600);
+        frame.setLocationRelativeTo(null);
         frame.setLayout(new BorderLayout());
         statusBar = new IDEStatusBar(this);
         frame.add(statusBar, BorderLayout.NORTH);
