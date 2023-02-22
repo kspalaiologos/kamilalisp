a49cdde7d6804effd238aef42561e0d253f0eeae
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-22 13:50:46 +0100

    union, unique: string overloads, preserve ordering

--- src/main/java/palaiologos/kamilalisp/runtime/array/Union.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/Union.java
@@ -1,12 +1,11 @@
 package palaiologos.kamilalisp.runtime.array;
 
-import palaiologos.kamilalisp.atom.Atom;
-import palaiologos.kamilalisp.atom.Environment;
-import palaiologos.kamilalisp.atom.Lambda;
-import palaiologos.kamilalisp.atom.PrimitiveFunction;
+import palaiologos.kamilalisp.atom.*;
+import palaiologos.kamilalisp.error.TypeError;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class Union extends PrimitiveFunction implements Lambda {
@@ -17,9 +16,24 @@ public class Union extends PrimitiveFunction implements Lambda {
 
     @Override
     public Atom apply(Environment env, List<Atom> args) {
-        HashSet<Atom> set = new HashSet<>();
-        for (Atom arg : args)
-            set.addAll(arg.getList());
-        return new Atom(new ArrayList<>(set));
+        if(args.isEmpty())
+            return Atom.NULL;
+        else if(args.get(0).getType() == Type.STRING) {
+            LinkedHashSet<Character> set = new LinkedHashSet<>();
+            for (Atom arg : args)
+                for (char c : arg.getString().toCharArray())
+                    set.add(c);
+            StringBuilder sb = new StringBuilder();
+            for (char c : set)
+                sb.append(c);
+            return new Atom(sb.toString());
+        } else if(args.get(0).getType() == Type.LIST) {
+            LinkedHashSet<Atom> set = new LinkedHashSet<>();
+            for (Atom arg : args)
+                set.addAll(arg.getList());
+            return new Atom(new ArrayList<>(set));
+        } else {
+            throw new TypeError("union: invalid argument type, expected string or list.");
+        }
     }
 }
--- src/main/java/palaiologos/kamilalisp/runtime/array/Unique.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/Unique.java
@@ -1,12 +1,11 @@
 package palaiologos.kamilalisp.runtime.array;
 
-import palaiologos.kamilalisp.atom.Atom;
-import palaiologos.kamilalisp.atom.Environment;
-import palaiologos.kamilalisp.atom.Lambda;
-import palaiologos.kamilalisp.atom.PrimitiveFunction;
+import palaiologos.kamilalisp.atom.*;
+import palaiologos.kamilalisp.error.TypeError;
 
 import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.List;
 
 public class Unique extends PrimitiveFunction implements Lambda {
@@ -14,9 +13,22 @@ public class Unique extends PrimitiveFunction implements Lambda {
     @Override
     public Atom apply(Environment env, List<Atom> args) {
         assertArity(args, 1);
-        List<Atom> list = args.get(0).getList();
-        HashSet<Atom> set = new HashSet<>(list);
-        return new Atom(new ArrayList<>(set));
+        if(args.get(0).getType() == Type.LIST) {
+            List<Atom> list = args.get(0).getList();
+            LinkedHashSet<Atom> set = new LinkedHashSet<>(list);
+            return new Atom(new ArrayList<>(set));
+        } else if(args.get(0).getType() == Type.STRING) {
+            String str = args.get(0).getString();
+            LinkedHashSet<Character> set = new LinkedHashSet<>();
+            for (char c : str.toCharArray())
+                set.add(c);
+            StringBuilder sb = new StringBuilder();
+            for (char c : set)
+                sb.append(c);
+            return new Atom(sb.toString());
+        } else {
+            throw new TypeError("unique: invalid argument type, expected string or list.");
+        }
     }
 
     @Override
