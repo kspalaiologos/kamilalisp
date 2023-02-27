d2ed7621249aec7efb5fcbce65ea2b80016d8123
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-02-27 13:25:40 +0100

    `import`: less strict rules, can load IDE project files :)

--- src/main/java/palaiologos/kamilalisp/runtime/meta/Import.java
+++ src/main/java/palaiologos/kamilalisp/runtime/meta/Import.java
@@ -2,13 +2,28 @@ package palaiologos.kamilalisp.runtime.meta;
 
 import palaiologos.kamilalisp.atom.*;
 import palaiologos.kamilalisp.repl.Main;
+import palaiologos.kamilalisp.runtime.ide.project.ProjectDataRegistry;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.util.List;
 
 public class Import extends PrimitiveFunction implements Lambda {
+    private boolean isProjectFile(String fileName) {
+        try {
+            FileInputStream fis = new FileInputStream(fileName);
+            byte[] magic = new byte[8];
+            fis.read(magic);
+            fis.close();
+            return new String(magic).equals("@KLP-BIN");
+        } catch (IOException e) {
+            return false;
+        }
+    }
+
     @Override
     public Atom apply(Environment env, List<Atom> args) {
         try {
@@ -16,7 +31,16 @@ public class Import extends PrimitiveFunction implements Lambda {
             Atom arg = args.get(0);
             arg.assertTypes(Type.STRING);
             String fileName = arg.getString();
-            String code = Files.readString(new File(fileName).getAbsoluteFile().toPath());
+            String code;
+            if(!isProjectFile(fileName)) {
+                code = Files.readString(new File(fileName).getAbsoluteFile().toPath());
+            } else {
+                ProjectDataRegistry registry = new ProjectDataRegistry(null);
+                registry.readFrom(new FileInputStream(fileName));
+                ByteArrayOutputStream baos = new ByteArrayOutputStream();
+                registry.serialiseTo(baos);
+                code = baos.toString();
+            }
             Environment childEnv = new Environment(Main.defaultRegistry);
             List<Atom> data = Parser.parse(code);
             for (int i = 0; i < data.size() - 1; i++) {
@@ -24,10 +48,10 @@ public class Import extends PrimitiveFunction implements Lambda {
             }
             Atom result = Evaluation.evaluate(childEnv, data.get(data.size() - 1));
             for (String key : childEnv.keys())
-                if (!env.has(key) && key.startsWith("public:"))
+                if (key.startsWith("public:"))
                     env.set(key.substring(7), childEnv.get(key));
             return result;
-        } catch (IOException e) {
+        } catch (IOException | ClassNotFoundException e) {
             throw new RuntimeException(e);
         }
     }
