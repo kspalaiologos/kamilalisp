a62a2dee1e360ec585e012fe35c691484f0f2a00
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-03-11 20:47:02 +0100

    and/or: more reasonable pervasivity

--- src/main/java/palaiologos/kamilalisp/runtime/math/Or.java
+++ src/main/java/palaiologos/kamilalisp/runtime/math/Or.java
@@ -2,13 +2,14 @@ package palaiologos.kamilalisp.runtime.math;
 
 import com.google.common.collect.Streams;
 import palaiologos.kamilalisp.atom.*;
+import palaiologos.kamilalisp.error.TypeError;
 
 import java.util.List;
 
 public class Or extends PrimitiveFunction implements Lambda {
-    private static Atom min2(Environment e, Atom a, Atom b) {
+    private static Atom or2(Atom a, Atom b) {
         if (a.getType() == Type.LIST && b.getType() == Type.LIST) {
-            return new Atom(Streams.zip(a.getList().stream(), b.getList().stream(), (x, y) -> min2(e, x, y)).toList());
+            return new Atom(Streams.zip(a.getList().stream(), b.getList().stream(), Or::or2).toList());
         } else {
             return new Atom(a.coerceBool() || b.coerceBool());
         }
@@ -21,21 +22,12 @@ public class Or extends PrimitiveFunction implements Lambda {
 
     @Override
     public Atom apply(Environment env, List<Atom> args) {
-        if (args.isEmpty())
-            throw new RuntimeException("or called with no arguments.");
-
-        if (args.size() == 1 && args.get(0).getType() == Type.LIST) {
-            List<Atom> data = args.get(0).getList();
-
-            for (int i = 0; i < data.size() - 1; i++)
-                if (min2(env, data.get(i), data.get(i + 1)).coerceBool())
-                    return Atom.TRUE;
-            return Atom.FALSE;
+        if (args.size() == 2) {
+            return or2(args.get(0), args.get(1));
+        } else if (args.size() <= 1) {
+            throw new TypeError("Expected 2 or more arguments to `or'.");
+        } else {
+            return args.stream().reduce(Or::or2).get();
         }
-
-        for (int i = 0; i < args.size() - 1; i++)
-            if (min2(env, args.get(i), args.get(i + 1)).coerceBool())
-                return Atom.TRUE;
-        return Atom.FALSE;
     }
 }
