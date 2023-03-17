04d35be009c2af457dcaa1b347d52dc9216d3869
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-03-17 20:40:29 +0100

    SQL: prep stmt

--- src/main/java/palaiologos/kamilalisp/runtime/sql/SQLConnectionUserdata.java
+++ src/main/java/palaiologos/kamilalisp/runtime/sql/SQLConnectionUserdata.java
@@ -25,8 +25,12 @@ public class SQLConnectionUserdata implements Userdata {
         switch (keyStr) {
             case "statement":
                 return new Atom(new SQLStatement());
+            case "prepare-statement":
+                return new Atom(new SQLPrepareStatement());
             case "close":
                 return new Atom(new SQLClose());
+            case "commit":
+                return new Atom(new SQLCommit());
             default:
                 throw new RuntimeException("sql:connection - no such field: " + keyStr);
         }
@@ -59,6 +63,44 @@ public class SQLConnectionUserdata implements Userdata {
         @Override
         protected String name() {
             return "sql:statement";
+        }
+    }
+
+    private class SQLCommit extends PrimitiveFunction implements Lambda {
+        @Override
+        public Atom apply(Environment env, List<Atom> args) {
+            assertArity(args, 0);
+            try {
+                conn.commit();
+            } catch (SQLException e) {
+                throw new RuntimeException(e);
+            }
+            return Atom.NULL;
+        }
+
+        @Override
+        protected String name() {
+            return "sql:commit";
+        }
+    }
+
+    private class SQLPrepareStatement extends PrimitiveFunction implements Lambda {
+        @Override
+        public Atom apply(Environment env, List<Atom> args) {
+            assertArity(args, 2);
+            Callable callback = args.get(1).getCallable();
+            Atom result;
+            try(var stmt = conn.prepareStatement(args.get(0).getString())) {
+                result = callback.apply(env, List.of(new Atom(new SQLPreparedStatementUserdata(stmt))));
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+            return result;
+        }
+
+        @Override
+        protected String name() {
+            return "sql:prepare-statement";
         }
     }
 
