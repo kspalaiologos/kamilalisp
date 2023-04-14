2db78e776999d4e386a6b560e28d1d13447e0b25
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-04-14 08:37:35 +0200

    chordality check for graphs

--- src/main/java/palaiologos/kamilalisp/runtime/lib/AlgorithmLib.java
+++ src/main/java/palaiologos/kamilalisp/runtime/lib/AlgorithmLib.java
@@ -139,6 +139,7 @@ public class AlgorithmLib {
         env.setPrimitive("graph:bron-kerbosch-clique-finder", new Atom(new BronKerboschCliqueFinder()));
         env.setPrimitive("graph:chordal-graph-maximum-clique-finder", new Atom(new ChordalGraphMaximumCliqueFinder()));
         env.setPrimitive("graph:chinese-postman", new Atom(new ChinesePostman()));
+        env.setPrimitive("graph:is-chordal-graph", new Atom(new ChordalityCheck()));
 
         env.setPrimitive("regex:matches?", "⍫⊖∊?", new Atom(new RegexMatches()));
         env.setPrimitive("regex:replace", "⍫⊖⍆", new Atom(new RegexReplace()));
