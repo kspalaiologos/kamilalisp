69778de7af43a2c8d07da7b73d30ded8ba74b1d0
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-07-05 15:18:57 +0200

    index-of: find all indices of

--- src/main/java/palaiologos/kamilalisp/runtime/array/IndexOf.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/IndexOf.java
@@ -3,14 +3,30 @@ package palaiologos.kamilalisp.runtime.array;
 import palaiologos.kamilalisp.atom.*;
 
 import java.math.BigInteger;
+import java.util.ArrayList;
 import java.util.List;
 
 public class IndexOf extends PrimitiveFunction implements Lambda {
     private static Atom indexOf(Atom a, Atom b) {
         if (a.getType() == Type.STRING && b.getType() == Type.STRING) {
-            return new Atom(BigInteger.valueOf(b.getString().indexOf(a.getString())));
+            String haystack = b.getString();
+            String needle = a.getString();
+            int index = haystack.indexOf(needle);
+            List<Atom> list = new ArrayList<>();
+            while (index >= 0) {
+                list.add(new Atom(BigInteger.valueOf(index)));
+                index = haystack.indexOf(needle, index + 1);
+            }
+            return new Atom(list);
         } else if (b.getType() == Type.LIST) {
-            return new Atom(BigInteger.valueOf(b.getList().indexOf(a)));
+            List<Atom> haystack = b.getList();
+            int index = haystack.indexOf(a);
+            List<Atom> list = new ArrayList<>();
+            while (index >= 0) {
+                list.add(new Atom(BigInteger.valueOf(index)));
+                index = haystack.subList(index + 1, haystack.size()).indexOf(a);
+            }
+            return new Atom(list);
         } else {
             throw new RuntimeException("index-of takes an atom and a list or two strings as arguments");
         }
