f0aef41d9a8f22e54ed72ad2f44461a961429a31
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-07-15 22:15:56 +0200

    remove and retire remaining usages of iterator pipelines

--- src/main/java/palaiologos/kamilalisp/runtime/graph/RS.java
+++ src/main/java/palaiologos/kamilalisp/runtime/graph/RS.java
@@ -1,5 +1,6 @@
 package palaiologos.kamilalisp.runtime.graph;
 
+import org.jgrapht.traverse.DepthFirstIterator;
 import org.jgrapht.traverse.RandomWalkVertexIterator;
 import palaiologos.kamilalisp.atom.*;
 
@@ -8,17 +9,22 @@ import java.util.List;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-public class RS extends PrimitiveFunction implements SpecialForm {
+public class RS extends PrimitiveFunction implements Lambda {
     @Override
     public Atom apply(Environment env, List<Atom> args) {
-        assertArity(args, 3);
-        GraphWrapper w = Evaluation.evaluate(env, args.get(0)).getUserdata(GraphWrapper.class);
+        GraphWrapper w = args.get(0).getUserdata(GraphWrapper.class);
         Atom start = args.get(1);
-        List<Atom> ops = args.get(2).getList();
+        Callable op = args.get(2).getCallable();
+        Atom init = args.size() > 3 ? args.get(3) : Atom.NULL;
         Iterator<Atom> it = new RandomWalkVertexIterator<>(w.getGraph(), start);
-        Iterable<Atom> iterable = () -> it;
-        Stream<Atom> stream = StreamSupport.stream(iterable.spliterator(), false);
-        return IteratorPipeline.evaluate(env, ops, stream);
+        while (it.hasNext()) {
+            Atom a = it.next();
+            List<Atom> result = op.apply(env, List.of(a, init)).getList();
+            init = result.get(0);
+            if (result.get(1).coerceBool())
+                break;
+        }
+        return init;
     }
 
     @Override
--- src/main/java/palaiologos/kamilalisp/runtime/graph/TOS.java
+++ src/main/java/palaiologos/kamilalisp/runtime/graph/TOS.java
@@ -1,5 +1,6 @@
 package palaiologos.kamilalisp.runtime.graph;
 
+import org.jgrapht.traverse.DepthFirstIterator;
 import org.jgrapht.traverse.TopologicalOrderIterator;
 import palaiologos.kamilalisp.atom.*;
 
@@ -9,24 +10,36 @@ import java.util.List;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
-public class TOS extends PrimitiveFunction implements SpecialForm {
+public class TOS extends PrimitiveFunction implements Lambda {
     @Override
     public Atom apply(Environment env, List<Atom> args) {
         if(args.size() == 3) {
-            GraphWrapper w = Evaluation.evaluate(env, args.get(0)).getUserdata(GraphWrapper.class);
+            GraphWrapper w = args.get(0).getUserdata(GraphWrapper.class);
             Callable comparator = Evaluation.evaluate(env, args.get(1)).getCallable();
-            List<Atom> ops = args.get(2).getList();
+            Callable op = args.get(2).getCallable();
+            Atom init = args.size() > 3 ? args.get(3) : Atom.NULL;
             Iterator<Atom> it = new TopologicalOrderIterator<>(w.getGraph(), (o1, o2) -> Evaluation.evaluate(env, comparator, List.of(o1, o2)).getInteger().intValueExact());
-            Iterable<Atom> iterable = () -> it;
-            Stream<Atom> stream = StreamSupport.stream(iterable.spliterator(), false);
-            return IteratorPipeline.evaluate(env, ops, stream);
+            while (it.hasNext()) {
+                Atom a = it.next();
+                List<Atom> result = op.apply(env, List.of(a, init)).getList();
+                init = result.get(0);
+                if (result.get(1).coerceBool())
+                    break;
+            }
+            return init;
         } else if(args.size() == 2) {
-            GraphWrapper w = Evaluation.evaluate(env, args.get(0)).getUserdata(GraphWrapper.class);
-            List<Atom> ops = args.get(2).getList();
+            GraphWrapper w = args.get(0).getUserdata(GraphWrapper.class);
+            Callable op = args.get(1).getCallable();
+            Atom init = args.size() > 2 ? args.get(3) : Atom.NULL;
             Iterator<Atom> it = new TopologicalOrderIterator<>(w.getGraph(), Comparator.naturalOrder());
-            Iterable<Atom> iterable = () -> it;
-            Stream<Atom> stream = StreamSupport.stream(iterable.spliterator(), false);
-            return IteratorPipeline.evaluate(env, ops, stream);
+            while (it.hasNext()) {
+                Atom a = it.next();
+                List<Atom> result = op.apply(env, List.of(a, init)).getList();
+                init = result.get(0);
+                if (result.get(1).coerceBool())
+                    break;
+            }
+            return init;
         } else
             throw new RuntimeException("Invalid arity");
     }
