94113d76ab1d080f064c83f5417751855bbf48aa
Kamila Szewczyk <kspalaiologos@gmail.com>
2023-07-16 13:02:51 +0200

    group functors

--- src/main/java/palaiologos/kamilalisp/runtime/array/Group.java
+++ src/main/java/palaiologos/kamilalisp/runtime/array/Group.java
@@ -11,13 +11,21 @@ import java.util.Map;
 public class Group extends PrimitiveFunction implements Lambda {
     @Override
     public Atom apply(Environment env, List<Atom> args) {
-        if (args.size() != 1) {
-            throw new RuntimeException("Wrong number of arguments to group - expected 1, got " + args.size());
+        if (args.size() != 1 && args.size() != 2) {
+            throw new RuntimeException("Wrong number of arguments to group - expected 1 or 2, got " + args.size());
         }
 
         List<Atom> data;
-        args.get(0).assertTypes(Type.LIST);
-        data = args.get(0).getList();
+        Callable functor = null;
+        if(args.size() == 1) {
+            args.get(0).assertTypes(Type.LIST);
+            data = args.get(0).getList();
+        } else {
+            args.get(0).assertTypes(Type.CALLABLE);
+            args.get(1).assertTypes(Type.LIST);
+            functor = args.get(0).getCallable();
+            data = args.get(1).getList();
+        }
 
         HashMap<Atom, ArrayList<Atom>> counts = new HashMap<>();
         for (int i = 0; i < data.size(); i++) {
@@ -31,12 +39,19 @@ public class Group extends PrimitiveFunction implements Lambda {
             }
         }
 
-        ArrayList<Atom> list = new ArrayList<>();
-        for (Map.Entry<Atom, ArrayList<Atom>> x : counts.entrySet()) {
-            Atom atom = new Atom(List.of(x.getKey(), new Atom(x.getValue())));
-            list.add(atom);
+        if(functor == null) {
+            ArrayList<Atom> list = new ArrayList<>();
+            for (Map.Entry<Atom, ArrayList<Atom>> x : counts.entrySet()) {
+                Atom atom = new Atom(List.of(x.getKey(), new Atom(x.getValue())));
+                list.add(atom);
+            }
+            return new Atom(list);
+        } else {
+            ArrayList<Atom> list = new ArrayList<>();
+            for (Map.Entry<Atom, ArrayList<Atom>> x : counts.entrySet())
+                list.add(Evaluation.evaluate(env, functor, List.of(x.getKey(), new Atom(x.getValue()))));
+            return new Atom(list);
         }
-        return new Atom(list);
     }
 
     @Override
